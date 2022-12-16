019cbddd09f975cde85ec078b93da4aaae135978
cdklat@gmail.com <cdklat@gmail.com>
2022-12-16 14:05:03 +0700

    JS

---
+++ Javascript/indexjsstrukturdata.js
@@ -0,0 +1,13 @@
+const response = {
+    statusCode: 200,
+    endpoint: 'GET /api/v1/dummies',
+    body: {
+      message: 'Hello World',
+      data: [
+        { id: 1, name: 'Andre', age: 20 },
+        { id: 2, name: 'Bella', age: 21 },
+     ],
+    totalData: 100,
+    responseTime: '1506ms'
+ }
+}
